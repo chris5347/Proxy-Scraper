a80f19c55ca64d79e972e99eada60a3da373b80d
mrrobot <kooldude5347@ahoo.com>
2019-02-02 15:50:43 -0500

    adding files

---
+++ ProxyScraper/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace ProxyScraper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label37 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(4, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scrape HTTP/HTTPs Proxy";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Proxy Count:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.Location = new System.Drawing.Point(12, 46);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(291, 111);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(309, 46);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(184, 300);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "Proxy List Preview";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(154, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Download Proxy List";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(434, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(126)))), ((int)(((byte)(49)))));
+            this.panel1.Controls.Add(this.label37);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 40);
+            this.panel1.TabIndex = 15;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProxyScraper.Properties.Resources.close;
+            this.pictureBox1.Location = new System.Drawing.Point(454, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 34);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label37
+            // 
+            this.label37.AutoSize = true;
+            this.label37.Font = new System.Drawing.Font("Lucida Handwriting", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label37.ForeColor = System.Drawing.Color.White;
+            this.label37.Location = new System.Drawing.Point(12, 9);
+            this.label37.Name = "label37";
+            this.label37.Size = new System.Drawing.Size(168, 24);
+            this.label37.TabIndex = 2;
+            this.label37.Text = "Proxy Scraper";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 163);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 182);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Sites Scraped: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Time:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(91, 68);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(502, 357);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label37;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ProxyScraper/Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/**
+ * ProxyScraper --- This program gathers free proxies that are publicly avaliable on specific websites.
+ * @author    Chris Campone
+ */
+namespace ProxyScraper
+{
+    public partial class Form1 : Form
+    {
+        ArrayList proxyList = new ArrayList();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        //site 1
+        private async Task getProxyFromDailyProxy()
+        {
+            string link = "http://proxy-daily.com/";
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(link);//go to link
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();//read the source code of the website
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);//convert to plaintext just incase
+            await Task.Delay(200);
+            while (sr.Peek() >= 0)
+            {
+                try
+                {
+                    string line = sr.ReadLine();
+                    //make sure the the line in the source code is in fact a proxy. 
+                    if (line.Contains(":") && line.Contains(".") && !line.Contains(">") && !line.Contains("("))
+                    {
+                       proxyList.Add(line);
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("getProxyFromDailyProxy: "+e.ToString());
+                }
+
+            }
+            sr.Close();
+            myResponse.Close();
+            label2.Text = proxyList.Count.ToString();
+            cmd("**Daily-Proxy Scrapped**");
+        }
+
+        //site 2
+        private async void getProxyFromFreeProxyList()
+        {
+            string link = "https://free-proxy-list.net/";
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(link);//go to site
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();//get source code
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            await Task.Delay(200);
+            string all = sr.ReadToEnd();
+
+            try
+            {
+                while (all.Length > 0)
+                {
+                    //loop thru each line and keep stripping the html tags for each proxy
+
+                    all = all.Substring(all.IndexOf("<tr><td>"));//first proxy is found at these tags
+                    string ip = "x";
+                    string port = "x";
+                    all = all.Substring(8);  //How the source will look: 203.126.218.186</td><td>80</td><td>SG</td
+                    ip = all.Substring(0, all.IndexOf("</td><td>"));
+                    all = all.Substring(all.IndexOf("<td>") + 4);//get the port: 80</td><td>SG</td
+                    port = all.Substring(0, all.IndexOf("</td><td>"));
+                    proxyList.Add(ip+":"+port);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+            sr.Close();
+            myResponse.Close();
+            label2.Text = proxyList.Count.ToString();
+            cmd("**Free-Proxy Scrapped**");
+        }
+
+        //site 3
+        private async void getProxyFromHTMLTunnel()
+        {
+            try
+            {
+                cmd("Gathering proxies from HTTP Tunnel");
+                ArrayList links = new ArrayList();
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("http://www.httptunnel.ge/ProxyListForFree.aspx");
+                myRequest.Method = "GET";
+                WebResponse myResponse = myRequest.GetResponse();
+                StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+                await Task.Delay(200);
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    string s = "";
+                    //for this source, each html proxy should contain hyperlink
+                    if (line.Contains("HyperLink2"))
+                    {
+                        s = line.Substring(line.IndexOf("target=\"_new\"") + 14);//232332:8080</a>
+                        s = s.Substring(0, s.Length - 4);
+                        proxyList.Add(s);
+                    }
+                }
+                if (textBox3.Lines.Length >= 6) textBox3.Text = "";
+                label2.Text = proxyList.Count.ToString();
+                cmd("**HTTP Tunnel Scrapped**");
+                sr.Close();
+                myResponse.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("HTML Tunnel: "+e.ToString());
+            }
+        }
+
+        //site 4
+        private  async void getProxyFromProxyServerList()
+        {
+            string link = "http://www.proxyserverlist24.top/";
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(link);
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            await Task.Delay(200);
+            string all = sr.ReadToEnd();
+
+            try
+            {
+                //this site has multiple pages that we must increment
+                ArrayList proxylinks = new ArrayList();
+                while(all.Contains("post-title entry-title")){
+                    all = all.Substring(all.IndexOf("post-title entry-title"));
+                    all = all.Substring(all.IndexOf("href=") + 6);
+                    string plink = all.Substring(0, all.IndexOf("'"));
+                    if(!plink.ToLower().Contains("smtp")){
+                        proxylinks.Add(plink);
+                    }
+                }
+
+                foreach (var item in proxylinks)
+                {
+                    myRequest = (HttpWebRequest)WebRequest.Create(item.ToString());
+                    myRequest.Method = "GET";
+                    myResponse = myRequest.GetResponse();
+                    sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+                    await Task.Delay(200);
+                    while (sr.Peek() >= 0)
+                    {
+                        try
+                        {
+                            string line = sr.ReadLine();
+
+                            if (line.Contains(":") && line.Contains(".") && !line.Contains(" "))
+                            {
+                                proxyList.Add(line);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBox.Show("getProxyFromProxyServerList: " + e.ToString());
+                        }
+
+                    }
+
+                }
+
+               
+            }
+            catch (Exception)
+            {
+
+            }
+            sr.Close();
+            myResponse.Close();
+            label2.Text = proxyList.Count.ToString();
+            cmd("**Proxy Server Scrapped**");
+        }
+
+        async private Task run()
+        {
+            proxyList.Clear();
+            cmd("Gathering proxies");
+            label2.Text = "0";
+            label4.Text = "0";
+            label6.Text = "0";
+            timer1.Start();
+            timerCount = 0;
+            int sitesScraped = 0;
+            try
+            {
+                
+                getProxyFromFreeProxyList();
+                sitesScraped++;
+                label4.Text = sitesScraped.ToString();
+                await Task.Delay(400);
+                getProxyFromHTMLTunnel();
+                sitesScraped++;
+                label4.Text = sitesScraped.ToString();
+                await Task.Delay(400);
+                await getProxyFromDailyProxy();
+                sitesScraped++;
+                label4.Text = sitesScraped.ToString();
+                await Task.Delay(400);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("btn 1 click: " + x.ToString());
+            }
+            cmd("Getting rid of duplicates...");
+            await Task.Delay(100);
+            for (int i = 0; i < proxyList.Count; i++)
+            {
+                for (int j = i + 1; j < proxyList.Count; j++)
+                    if (proxyList[i].ToString() == proxyList[j].ToString())
+                        proxyList.Remove(proxyList[j]);
+            }
+
+
+            cmd("complete");
+            label2.Text = proxyList.Count.ToString();
+            textBox2.Text = "";
+            for (int i = 0; i < 20; i++)
+            {
+                textBox2.Text += proxyList[i] + Environment.NewLine;
+            }
+            button2.Enabled = true;
+            timer1.Stop();
+        }
+
+        //Run proxyscraper
+        async private void button1_Click(object sender, EventArgs e)
+        {
+            await run();
+        }
+
+       
+        private void writeFile(ArrayList array, string name)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            try
+            {
+                using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(path + @"\" + name + ".txt"))
+                {
+                    foreach (string line in array)
+                    {
+                        file.WriteLine(line);
+                    }
+                }
+                cmd("File saved on desktop: " + name + ".txt");
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        
+        private string findReplace(string Source, string Find, string Replace)
+        {
+            int Place = Source.IndexOf(Find);
+            string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+            return result;
+        }
+
+        //Save btn
+        private void button2_Click(object sender, EventArgs e)
+        {
+            writeFile(proxyList,"Fresh-Proxies");
+            button2.Enabled = false;
+        }
+
+        //clear btn
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "Proxy List Preview";
+            proxyList.Clear();
+            label2.Text = proxyList.Count.ToString();
+            cmd("Proxy list cleared");
+            button2.Enabled = false;
+            label2.Text = "0";
+            label4.Text = "0";
+            label6.Text = "0";
+        }
+
+        private void cmd(string x)
+        {
+            if(textBox3.Lines.Count() >= 7){
+                textBox3.Text = "";
+            }
+            textBox3.Text += x + Environment.NewLine;
+        }
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //This allows me to be able to drag the program from the top panel
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+        //keep track of time it takes to gather proxies
+        int timerCount = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerCount++;
+            label6.Text = timerCount+" seconds";
+        }
+
+
+    }
+}
